e5a13ec6afd735a842500a9623a25b6c19632aaf
David Leiva Roncero <david.leiva.roncero@gmail.com>
2023-04-05 13:11:23 +0200

    Added sprite movement and gravity

--- src/main.py
+++ src/main.py
@@ -7,10 +7,10 @@ import random
 pygame.init()
 window = pygame.display.set_mode((500, 800))
 pygame.display.set_caption("Tetris")
+clock = pygame.time.Clock()
 
 
 class Piece(sprite.Sprite):
-
     sprite_loc = {
         'square': (18, 18, 76, 76),
         's1': (180, 18, 114, 76),
@@ -28,35 +28,67 @@ class Piece(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.midtop = (window.get_width() / 2, 0)
 
-    def update(self):
+        self.speed = 10
+
+    def update(self, dt, w):
         pass
+
+    def move(self, x=0, y=0):
+        if self.rect.centerx + x >= window.get_width() or self.rect.centerx + x < 0:
+            return
+        if self.rect.bottom + y >= window.get_height() or self.rect.top + y < 0:
+            return
+        self.rect.center = (self.rect.centerx + x, self.rect.centery + y)
+
+    def fall(self, g=1):
+        self.move(0, self.speed * g)
 
 
 if __name__ == '__main__':
 
     piece = Piece(random.choice(list(Piece.sprite_loc.keys())))
     sprites_group = pygame.sprite.GroupSingle()
+    speed = piece.speed
     sprites_group.add(piece)
+    gravity = 1
+    elasticity = 1
 
     while True:
 
+        dt = clock.tick(30) / 1000
+        pixels_h = 0
         sprites_group.draw(window)
 
+        keys = pygame.key.get_pressed()
+        if keys[K_LEFT]:
+            elasticity *= 1.2
+            pixels_h = -speed
+        if keys[K_RIGHT]:
+            elasticity *= 1.2
+            pixels_h = speed
+        if keys[K_DOWN]:
+            gravity *= 2
+        if keys[K_ESCAPE]:
+            pygame.quit()
+            sys.exit()
+
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                keys = pygame.key.get_pressed()
-                if keys[K_w]:
-                    window.fill(pygame.Color("blue"))
-                if keys[K_a]:
-                    window.fill(pygame.Color("red"))
-                if keys[K_d]:
-                    window.fill(pygame.Color("green"))
-                if keys[K_ESCAPE]:
-                    pygame.quit()
-                    sys.exit()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN:
+                    gravity = 1
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    elasticity = 1
 
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-        pygame.display.flip()
+        for p in sprites_group:
+            p.move(pixels_h * elasticity)
+            p.fall(gravity)
+
+        window.fill((0, 0, 0))
+        sprites_group.update(dt, window)
+        sprites_group.draw(window)
+        pygame.display.update()
+
