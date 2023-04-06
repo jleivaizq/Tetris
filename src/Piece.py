baa0938f13860e2c7975950992b8ef974199e13b
David Leiva Roncero <david.leiva.roncero@gmail.com>
2023-04-06 18:54:29 +0200

    Adds Piece rotation feature

--- src/Piece.py
+++ src/Piece.py
@@ -7,13 +7,13 @@ from pygame import sprite, Surface
 class Piece(sprite.Sprite):
 
     sprite_loc = {
-        'square': (18, 18, 76, 76),
-        's1': (180, 18, 114, 76),
-        's2': (569, 0, 77, 112),
-        't': (0, 227, 112, 77),
-        'l1': (0, 411, 112, 77),
-        'l2': (0, 621, 112, 76),
-        'line': (36, 793, 39, 150),
+        'square': [(18, 18, 76, 76)],
+        's1': [(180, 18, 114, 76), (384, 0, 76, 112)],
+        's2': [(569, 0, 77, 112), (735, 18, 113, 76)],
+        't': [(0, 227, 112, 77), (193, 208, 76, 114), (379, 227, 114, 77), (558, 208, 77, 114)],
+        'l1': [(0, 411, 112, 77), (202, 392, 77, 114), (385, 411, 114, 77), (562, 392, 76, 114)],
+        'l2': [(0, 621, 112, 76), (199, 602, 77, 113), (382, 621, 114, 76), (577, 602, 77, 113)],
+        'line': [(36, 793, 39, 150), (193, 849, 151, 39)],
     }
 
     @staticmethod
@@ -23,8 +23,10 @@ class Piece(sprite.Sprite):
     def __init__(self, piece_type: str, window: Surface):
         sprite.Sprite.__init__(self)
         self.window = window
+        self.piece_type = piece_type
+        self.current_pos = 0
         self.spriteSheet = pygame.image.load('sprites/sheet.png').convert_alpha()
-        self.image = self.spriteSheet.subsurface(Piece.sprite_loc[piece_type])
+        self.image = self.spriteSheet.subsurface(Piece.sprite_loc[self.piece_type][self.current_pos])
         self.rect = self.image.get_rect()
         self.rect.midtop = (window.get_width() / 2, 0)
 
@@ -51,3 +53,8 @@ class Piece(sprite.Sprite):
 
     def fall(self, g=1):
         self.move(0, self.speed * g)
+
+    def rotate(self, clock_wise: bool = True):
+        list_pos: list = Piece.sprite_loc[self.piece_type]
+        self.current_pos = (self.current_pos + 1 if clock_wise else self.current_pos - 1) % len(list_pos)
+        self.image = self.spriteSheet.subsurface(list_pos[self.current_pos])
